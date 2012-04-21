d150e45f3cc403a2802ae3ef626e5c501a90f966
richo <richo@psych0tik.net>
2012-04-22 01:29:19 +1000

    Fix mass brokenness in the router

--- router.py
+++ router.py
@@ -3,6 +3,8 @@ import app.index as index
 import app.about as about
 import app.contact as contact
 
-app = webapp2.WSGIApplication([('/', index.App)])
-app = webapp2.WSGIApplication([('/about', about.App)])
-app = webapp2.WSGIApplication([('/contact', contact.App)])
+app = webapp2.WSGIApplication([
+  ('/', index.App),
+  ('/about', about.App),
+  ('/contact', contact.App)
+])
